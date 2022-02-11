24caaa1c3df2579101e22b886cd3c58c64f396a9
Eric Hackman <kwesihackman@gmail.com>
2022-02-11 11:06:24 +0000

    implemented home screen with flatlist for getting a ride and ordering food

---
+++ components/NavOptions.js
@@ -0,0 +1,60 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+import React from 'react';
+import { ASSETS_BASE_URL } from '../utils/Constants';
+import tw from 'tailwind-react-native-classnames';
+import { Icon } from 'react-native-elements';
+
+const data = [
+  {
+    id: '123',
+    title: 'Get a ride',
+    image: `${ASSETS_BASE_URL}/3pn`,
+    screen: 'MapScreen',
+  },
+  {
+    id: '456',
+    title: 'Order food',
+    image: `${ASSETS_BASE_URL}/28w`,
+    screen: 'EatsScreen',
+  },
+];
+
+const NavOptions = () => {
+  return (
+    <FlatList
+      data={data}
+      horizontal
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => (
+        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+          <View>
+            <Image
+              source={{ uri: item.image }}
+              style={{ width: 120, height: 120, resizeMode: 'contain' }}
+            />
+            <Text style={tw`text-left mt-2 text-lg font-semibold`}>
+              {item.title}
+            </Text>
+            <Icon
+              type="antdesign"
+              color="white"
+              name="arrowright"
+              style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+            />
+          </View>
+        </TouchableOpacity>
+      )}
+    />
+  );
+};
+
+export default NavOptions;
+
+const styles = StyleSheet.create({});
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,25 @@
-import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
+import { View, Image, StyleSheet, SafeAreaView } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
 import React from 'react';
+import NavOptions from '../components/NavOptions';
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={tw` text-red-500 p-10`}>HomeScreen</Text>
+    <SafeAreaView style={tw` bg-white h-full`}>
+      <View style={tw`p-5`}>
+        <Image
+          style={{
+            width: 100,
+            height: 100,
+            resizeMode: 'contain',
+          }}
+          source={{
+            uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Uber_logo_2018.svg/2560px-Uber_logo_2018.svg.png',
+          }}
+        />
+
+        <NavOptions />
+      </View>
     </SafeAreaView>
   );
 };
