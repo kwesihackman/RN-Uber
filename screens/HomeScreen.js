aa0037e98d274e763a3b904495b7b56eaddc366f
Eric Hackman <kwesihackman@gmail.com>
2022-02-11 10:12:24 +0000

    feat: installed tailwind

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,11 @@
 import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
+import tw from 'tailwind-react-native-classnames';
 import React from 'react';
 
 const HomeScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>HomeScreen</Text>
+      <Text style={tw` text-red-500 p-10`}>HomeScreen</Text>
     </SafeAreaView>
   );
 };
