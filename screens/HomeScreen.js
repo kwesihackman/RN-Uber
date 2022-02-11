840bcc8072a18489070a526450b15b60ab266c66
Eric Hackman <kwesihackman@gmail.com>
2022-02-11 10:08:26 +0000

    feat: implemented redux store using the redux toolkit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,18 @@
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
+import React from 'react';
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>HomeScreen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
